668faa61f5a2ae4986da96425b100ae411033957
Andres Guevara <=>
2022-06-08 22:42:04 -0500

    Update Auth's component css

--- dashboard-client/src/layouts/Auth/Auth.js
+++ dashboard-client/src/layouts/Auth/Auth.js
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
 import style from "./Auth.module.css";
 function Auth(){
     return (
-       <div className={style.container}>           
+       <div className={style.container}>    
+        <div className={style.innerContainer}>
+            <Outlet/>     
+        </div>          
        </div> 
     )
 }
