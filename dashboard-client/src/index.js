8a7d9f627f57199e426d2ed57960a161e913ff1d
Andres Guevara <=>
2022-06-08 21:14:08 -0500

    Configuration of / and /dashboard routes

--- dashboard-client/src/index.js
+++ dashboard-client/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import Auth from './layouts/Auth';
+import DashBoard from './layouts/DashBoard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>Hola!</h1>
-  </React.StrictMode>
-);
+root.render(    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Auth/>}>    
+        </Route>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <Route path='/dashboard' element={<DashBoard/>}>
+        </Route>
+      </Routes>      
+    </BrowserRouter>    
+);
