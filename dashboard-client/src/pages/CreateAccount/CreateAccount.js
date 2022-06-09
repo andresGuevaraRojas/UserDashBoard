c8ac5388217b0bba84db09e439f44b86c0c4bf84
Andres Guevara <=>
2022-06-08 22:07:06 -0500

    Creation of Login and CreateAccount pages

---
+++ dashboard-client/src/pages/CreateAccount/CreateAccount.js
@@ -0,0 +1,10 @@
+import style from "./CreateAccount.module.css";
+function CreateAccount(){
+    return(
+        <main className={style.container}>
+            <h1>CreateAccount!</h1>
+        </main>
+    )
+}
+
+export default CreateAccount;
---
+++ dashboard-client/src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import style from './Login.module.css';
+
+function Login(){
+    return(
+        <main className={style.container}>
+            <h1>Login!</h1>
+        </main>
+    )
+}
+
+export default Login
