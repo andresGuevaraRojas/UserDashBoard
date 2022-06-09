338c3311a94c3662cbe8f66d8c17ef5a9f1bc769
Andres Guevara <=>
2022-06-08 22:43:11 -0500

    Add Login an CreateAccoutn components to Router

--- dashboard-client/src/index.js
+++ dashboard-client/src/index.js
@@ -5,12 +5,17 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Auth from './layouts/Auth';
 import DashBoard from './layouts/DashBoard';
+import Login from './pages/Login';
+import CreateAccount from './pages/CreateAccount';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(    
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Auth/>}>    
+          <Route index element={<Login/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='createaccount' element={<CreateAccount/>}/>
         </Route>
 
         <Route path='/dashboard' element={<DashBoard/>}>
