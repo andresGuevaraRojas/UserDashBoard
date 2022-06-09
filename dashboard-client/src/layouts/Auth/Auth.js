028527cb494becc66829504cc3c1668c15d420ac
Andres Guevara <=>
2022-06-08 21:12:58 -0500

    Creation of layouts components

---
+++ dashboard-client/src/layouts/Auth/Auth.js
@@ -0,0 +1,10 @@
+import style from "./Auth.module.css";
+function Auth(){
+    return (
+       <div>
+           <h1>Auth!</h1>
+       </div> 
+    )
+}
+
+export default Auth;
---
+++ dashboard-client/src/layouts/DashBoard/DashBoard.js
@@ -0,0 +1,9 @@
+import style from './DashBoard.module.css'
+function DashBoard(){
+    return(
+        <div>
+            <h1>DashBoard!</h1>
+        </div>
+    )
+}
+export default DashBoard
