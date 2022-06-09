3e7a1cc0d12b599ab81adc615a1a5290a8788b23
Andres Guevara <=>
2022-06-08 22:52:52 -0500

    Updates styles of backgroud

--- dashboard-client/src/pages/Login/Login.js
+++ dashboard-client/src/pages/Login/Login.js
@@ -3,7 +3,7 @@ import style from './Login.module.css';
 function Login(){
     return(
         <main className={style.container}>
-            <h1>Login!</h1>
+            <h1>Bienvenido</h1>
         </main>
     )
 }
