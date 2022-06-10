71887f16a91d9970ed799ca0221f59901417b449
Andres Guevara <=>
2022-06-09 21:13:06 -0500

    Add loginForm to Login page

--- dashboard-client/src/pages/Login/Login.js
+++ dashboard-client/src/pages/Login/Login.js
@@ -1,9 +1,19 @@
+import { Link } from 'react-router-dom';
+import LoginForm from '../../components/LoginForm';
 import style from './Login.module.css';
 
 function Login(){
+    const submit = async(values)=>{
+        console.log(values)
+    }
     return(
         <main className={style.container}>
-            <h1>Bienvenido</h1>
+            <h1 className={style.item}>Bienvenido</h1>
+            <LoginForm className={style.item} onFinish={submit}/>
+            <div className={`${style.createAccount} ${style.item}`}>
+                <span>¿No tienes cuenta?</span>
+                <Link to={'/createAccount'}>Registrarme</Link>
+            </div>
         </main>
     )
 }
